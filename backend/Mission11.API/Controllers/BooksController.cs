36586fea5f6af3fe04c3aaa69b6bee634fdee118
hadleysg <hadleygarff@gmail.com>
2025-03-19 20:06:48 -0600

    Full code - Hadley Garff

---
+++ backend/Mission11.API/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mission11.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mission11.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookstoreContext _context;
+
+        public BooksController(BookstoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+    }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import BookList from './BookList';
+
+// Hadley Garff 4-14
+// This app shows a list of books that can be sorted from a-z based on the title
+function App() {
+  return (
+    <div className="text-center">
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
